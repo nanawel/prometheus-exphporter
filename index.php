f9da7188d1062fff660f54dc0dc8d56473d70373
nanawel <nanawel@gmail.com>
2021-10-02 17:27:19 +0200

    Fix index.php execution when in CLI

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require __DIR__ . '/vendor/autoload.php';
 define('EXPHPORTER_BASE_DIR', __DIR__);
 
 // Always redirect to /metrics
-if ($_SERVER['REQUEST_URI'] != '/metrics') {
+if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] != '/metrics') {
     header('Location: /metrics');
     http_response_code(301);
     return;
